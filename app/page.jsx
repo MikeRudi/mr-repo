5f75a64ef1b9f734b4bae0e99f7403f5a83eb0e7
mike <mikerudyharris@gmail.com>
2026-05-19 14:31:44 +0200

    Add locale routing support

--- app/page.jsx
+++ app/page.jsx
@@ -1,33 +1,5 @@
-import LanguageController from "./components/LanguageController";
-import AnimationScripts from "./components/AnimationScripts";
-import VideoPopup from "./sections/VideoPopup";
-import Navigation from "./sections/Navigation";
-import HeroSection from "./sections/HeroSection";
-import VideoSection from "./sections/VideoSection";
-import LoungeOfTheYear from "./sections/LoungeOfTheYear";
-import AwardsShowcase from "./sections/AwardsShowcase";
-import OneToWatch from "./sections/OneToWatch";
-import FinalistsSection from "./sections/FinalistsSection";
-import Footer from "./sections/Footer";
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  return (
-    <>
-      <LanguageController />
-      <VideoPopup />
-      <div className="page-wrap">
-        <div className="page-main">
-          <Navigation />
-          <HeroSection />
-          <VideoSection />
-          <LoungeOfTheYear />
-          <AwardsShowcase />
-          <OneToWatch />
-          <FinalistsSection />
-          <Footer />
-        </div>
-      </div>
-      <AnimationScripts />
-    </>
-  );
+export default function RootPage() {
+  redirect("/en-GB");
 }
